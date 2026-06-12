67e75b5b2b6d08bce5aa0f45c47d1dbbfdf35716
Akummade <fe90pe2010@gmail.com>
2026-06-12 09:58:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,23 @@ def arquivoprincipal():
         "status":"online",
         "projeto":"Bolão Copa 2026"
     }
+    
+from database.connection import get_connection
+
+@app.route("/db-test")
+def db_test():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT 1")
+
+    resultado = cursor.fetchone()
+
+    conn.close()
+
+    return {
+        "database": "conectado",
+        "resultado": resultado
+    }
